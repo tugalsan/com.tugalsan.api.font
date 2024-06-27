ce17f893c26fc15135cdd62be4216364c6f59370
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:09:17 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,7 @@ module com.tugalsan.api.font {
     requires com.tugalsan.api.thread;
     requires com.tugalsan.api.tuple;
     requires com.tugalsan.api.stream;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     
     exports com.tugalsan.api.font.client;
     exports com.tugalsan.api.font.server;
